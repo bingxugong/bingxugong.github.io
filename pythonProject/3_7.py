1947bff4607b4d37403115d7d0cee77fa1f681a9
bingxugong <146114128+bingxugong@users.noreply.github.com>
2024-04-02 16:29:55 +0800

    Add files via upload

---
+++ pythonProject/3_7.py
@@ -0,0 +1,18 @@
+person = {
+    "name": "Alice",
+    "age": 30,
+    "occupation": "Engineer",
+    "location": "New York",
+    "interests": ["Reading", "Traveling", "Cooking"]
+}
+
+print(person['name'])
+#输出  Alice
+menu = {
+    "appetizers": ["Caesar Salad", "Spring Rolls", "Bruschetta"],
+    "main_courses": ["Grilled Salmon", "Chicken Alfredo", "Vegetable Stir-Fry"],
+    "desserts": ["Chocolate Cake", "Fruit Tart", "Ice Cream"]
+}
+
+print(menu['appetizers'])
+# 输出 ['Caesar Salad', 'Spring Rolls', 'Bruschetta']
